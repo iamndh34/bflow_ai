b6b3b6e93d95cb464e808a0edada996fa3917ec4
haind <haind@mtsolution.com.vn>
2025-11-05 10:00:23 +0700

    [master] update req.txt

--- mis_ai/apps/bflow_invoice_orc/apps.py
+++ mis_ai/apps/bflow_invoice_orc/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BflowInvoiceOrcConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bflow_invoice_orc'
+    name = 'apps.bflow_invoice_orc'
